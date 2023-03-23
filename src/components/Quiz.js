7d15bf67356ea6e62d0475d8cc5791b8d19a2e59
V S SRINIDHI <srinidhivs28@gmail.com>
2023-03-24 02:05:49 +0530

    Final Commit

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,20 +1,25 @@
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
-import '../styles/quiz.css'
+import '../styles/quiz.css';
+import Spinner from '../components/Spinner';
 
 const Quiz = () => {
   const [countries, setCountries] = useState([]);
   const [currentCountry, setCurrentCountry] = useState({});
   const [userInput, setUserInput] = useState('');
   const [isCorrect, setIsCorrect] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   const inputRef = useRef();
 
   useEffect(() => {
     axios.get('https://countriesnow.space/api/v0.1/countries/capital')
-      .then(response => {setCountries(response.data.data)
-        inputRef.current.focus()})
+      .then(response => {
+        setCountries(response.data.data);
+        inputRef.current.focus();
+      })
       .catch(error => console.error(error));
+    setIsLoading(false);
   }, []);
 
   useEffect(() => {
@@ -44,24 +49,24 @@ const Quiz = () => {
     setIsCorrect(null);
   };
 
-  return (
+  return isLoading ? <Spinner /> : (
     <div className='container'>
       <h1>Hi Sam😄, Guess the Capital City🗺️</h1>
       {currentCountry.name && (
-        <div>
+        <div className='sub-container'>
           <h2>What is the capital city of <span>{currentCountry.name}?</span></h2>
           <form onSubmit={handleSubmit}>
             <input type="text" value={userInput} onChange={handleInputChange} ref={inputRef} />
             <button type="submit" disabled={!userInput}>Check Answer</button>
           </form>
           {isCorrect === true && (
-            <p className='correct'>Correct🥳 </p>
+            <p className='correct'>Correct🥳</p>
           )}
           {isCorrect === false && (
-            <p className='incorrect'>Incorrect😞. The correct answer is <span>{currentCountry.capital}.</span></p>
+            <p className='incorrect'>Incorrect😞.The correct answer is <span>{currentCountry.capital}.</span></p>
           )}
           {isCorrect !== null && (
-            <button onClick={handleNextQuestion}>Next Question</button>
+            <button onClick={handleNextQuestion}>Next👉</button>
           )}
         </div>
       )}
